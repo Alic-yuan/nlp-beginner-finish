60049fcaeed00e0ece771300956065b89b707207
chenny3 <67456613+chenny3@users.noreply.github.com>
2020-08-20 14:13:19 +0800

    Update torch_test.py

--- task2/torch_test.py
+++ task2/torch_test.py
@@ -26,7 +26,7 @@ class CnnModel:
         self.words, self.word_to_id = read_vocab(vocab_dir)
         self.model = TextCNN()
         self.model.load_state_dict(torch.load('model_params.pkl'))
-        self.model.eval()
+        self.model.eval()  # 开启验证模式
 
     def predict(self, message):
         # 支持不论在python2还是python3下训练的模型都可以在2或者3的环境下运行
@@ -46,7 +46,7 @@ class RnnModel:
         self.words, self.word_to_id = read_vocab(vocab_dir)
         self.model = TextRNN()
         self.model.load_state_dict(torch.load('model_params.pkl'))
-        self.model.eval()
+        self.model.eval()  # 开启验证模式
 
     def predict(self, message):
         # 支持不论在python2还是python3下训练的模型都可以在2或者3的环境下运行
